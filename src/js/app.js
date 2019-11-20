d40e0bdef6d011d6d04be61b1337f40d0f2e8009
Jakub Figlak <jakub.figlak@gmail.com>
2019-11-20 19:05:32 +0100

    Install and test AXIOS library

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,12 @@
+import axios from 'axios';
+
+const endpoint = 'https://restcountries.eu/rest/v2/all';
+
 // build your app here
-export const app = () => console.log('hello world!');
+export const app = () => {
+    axios.get(endpoint)
+        .then(response => {
+            console.log(response.data);
+        })
+        .catch(error => console.log(error));
+}
