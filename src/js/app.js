02ca28213ef6508814e743b0ce4aa5b7dbe80e8a
Jakub Figlak <jakub.figlak@gmail.com>
2019-11-22 17:12:19 +0100

    Create basic html structure and css styling

--- src/js/app.js
+++ src/js/app.js
@@ -5,20 +5,20 @@ const endpoint = 'https://restcountries.eu/rest/v2/all';
 
 // build your app here
 export const app = () => {
-    axios.get(endpoint)
-        .then(response => {
-            console.log(response.data);
-        })
-        .catch(error => console.log(error));
+    // axios.get(endpoint)
+    //     .then(response => {
+    //         console.log(response.data);
+    //     })
+    //     .catch(error => console.log(error));
 
-    const swiper = new Swiper('.swiper-container', {
-        pagination: {
-            el: '.swiper-pagination',
-            type: 'fraction',
-        },
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-    });
+    // const swiper = new Swiper('.swiper-container', {
+    //     pagination: {
+    //         el: '.swiper-pagination',
+    //         type: 'fraction',
+    //     },
+    //     navigation: {
+    //         nextEl: '.swiper-button-next',
+    //         prevEl: '.swiper-button-prev',
+    //     },
+    // });
 }
