cc3c6969a3fcee2def5259b5071562eb093f0095
Jakub Figlak <jakub.figlak@gmail.com>
2019-11-23 14:21:03 +0100

    Create hamburger and scroll scripts

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
-import { app } from './js/app';
+import {
+    app
+} from './js/app';
+import {
+    hamburger
+} from './js/hamburger';
+import {
+    scrollOnClick
+} from './js/scroll';
 import './styles/main.scss';
 
 app();
+hamburger();
+scrollOnClick();
