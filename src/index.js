0fc101a97ed7c87df59f1df667eca901065d860f
Jakub Figlak <jakub.figlak@gmail.com>
2019-11-29 19:11:17 +0100

    Add quiz sections, refactor scroll function to work with parameters and add quiz sections

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import {
     hamburger
 } from './js/hamburger';
 import {
-    scrollOnClick
+    scrollToSection
 } from './js/scroll';
 import {
     searchCountries
@@ -14,5 +14,9 @@ import './styles/main.scss';
 
 app();
 hamburger();
-scrollOnClick();
+scrollToSection('.startBtn', '.country-search');
+scrollToSection('#home', '.welcome');
+scrollToSection('#search-info', '.country-search');
+scrollToSection('#capital-quiz', '.capital-quiz');
+scrollToSection('#flag-quiz', '.flag-quiz');
 searchCountries();
