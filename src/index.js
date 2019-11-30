6cea4e9af837c00f7911f699d40fa3d02ec0788e
Jakub Figlak <jakub.figlak@gmail.com>
2019-11-30 18:35:09 +0100

    Merge branch dev into test

--- src/index.js
+++ src/index.js
@@ -5,24 +5,6 @@ import {
     hamburger
 } from './js/hamburger';
 import {
-<<<<<<< HEAD
-    scrollToSection
-} from './js/scroll';
-import {
-    searchCountries,
-    carousel
-} from './js/searchCountries';
-import './styles/main.scss';
-import {
-    map
-} from './js/map';
-
-app();
-hamburger();
-scrollToSection('.startBtn', '.country-search');
-scrollToSection('.search-info', '.country-search');
-carousel();
-=======
     searchCountries
 } from './js/searchCountries';
 import './styles/main.scss';
@@ -46,4 +28,3 @@ generateTest();
 // runTest();
 displayTestOnClick();
 // checkResults();
->>>>>>> dev
--- src/js/searchCountries.js
+++ src/js/searchCountries.js
@@ -1,55 +1,6 @@
 // import axios from 'axios';
 // import Swiper from 'swiper';
 
-<<<<<<< HEAD
-// export const searchCountries = () => {
-//     const endpoint = 'https://restcountries.eu/rest/v2/name/';
-//     const searchInput = document.querySelector('.search');
-//     const countryCards = document.querySelector('.swiper-wrapper');
-
-//     async function getData() {
-//         try {
-//             if (this.value) {
-//                 const countries = [];
-//                 const filter = '?fields=name;flag;capital;region;subregion;area;population';
-//                 const searchWord = this.value;
-//                 const response = await axios.get(`${endpoint}${searchWord}${filter}`);
-//                 countries.push(...response.data);
-//                 console.log(countries);
-
-//                 const html = countries.map(country => {
-//                     return `<div class="country-card swiper-slide">
-//                     <div class="country-card-header">
-//                       <h2 class="country-name">${country.name}</h2>
-//                       <div class="flag">
-//                         <img src="${country.flag}" alt="${country.name} flag">
-//                       </div>
-//                     </div>
-//                     <ul class="country-facts">
-//                       <li>Capital city: ${country.capital}</li>
-//                       <li>Region: ${country.region}</li>
-//                       <li>Subregion: ${country.subregion}</li>
-//                       <li>Area: ${country.area}</li>
-//                       <li>Population: ${country.population}</li>
-//                     </ul>
-//                   </div>`
-//                 }).join('');
-//                 console.log(html);
-//                 countryCards.innerHTML = html;
-//                 const swiper = new Swiper('.swiper-container', {
-//                     navigation: {
-//                         nextEl: '.swiper-button-next',
-//                         prevEl: '.swiper-button-prev',
-//                     },
-//                 });
-//             }
-//         } catch (error) {
-//             console.error(error);
-//         }
-//     };
-
-//     searchInput.addEventListener('keyup', getData);
-=======
 export const searchCountries = () => {
   const endpoint = 'https://restcountries.eu/rest/v2/name/';
   const searchInput = document.querySelector('.search');
@@ -96,13 +47,5 @@ export const searchCountries = () => {
   };
 
   searchInput.addEventListener('keyup', getData);
->>>>>>> dev
 
-// }
-
-import materialize from 'materialize-css';
-
-export const carousel = () => {
-  const elems = document.querySelectorAll('.carousel');
-  const instances = M.Carousel.init(elems, options);
 }
